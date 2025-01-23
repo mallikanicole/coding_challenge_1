89ecfe0accf4425fef7949e30c5d5c02bbb2e6f8
mallikanicole <malmichael23@gmail.com>
2025-01-23 14:27:17 -0500

    added :::

--- cc_1.js
+++ cc_1.js
@@ -1,16 +1,16 @@
 let employeeName= "FirstName, LastName";//String-Name of employee.
 const employeeID= 101;//Number-ID assigned to employee.
 var isActive= true;//Boolean-True if employee is currently active.
-console.log("Employee Name", employeeName,typeof employeeName)
-console.log("Employee ID", employeeID,typeof employeeID)
-console.log("Active", isActive,typeof isActive)
+console.log("Employee Name:", employeeName,typeof employeeName)
+console.log("Employee ID:", employeeID,typeof employeeID)
+console.log("Active:", isActive,typeof isActive)
 console.log(employeeName,employeeID,isActive,)
 let productName= "ProductA";//String-Name of Product.
 const productPrice= "20";//Number-Price assigned to product.
 var isAvailable= true;//Boolean- True if product is currently available.
-console.log("Product Name", productName, typeof productName)
-console.log("Product Price", productPrice, typeof productPrice)
-console.log("Available", isAvailable, typeof isAvailable)
+console.log("Product Name:", productName, typeof productName)
+console.log("Product Price:", productPrice, typeof productPrice)
+console.log("Available:", isAvailable, typeof isAvailable)
 let accountBalance=500000;//Number-dollar amount in account
 const productCost= "5"
 let supplyAmount=6000;//Number-amount of supplies purchased
