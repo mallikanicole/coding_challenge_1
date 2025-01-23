f687606af0b0a71e5f65bb9141223f8423c985c7
mallikanicole <malmichael23@gmail.com>
2025-01-22 20:16:55 -0500

    Task 1-Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,6 +1,9 @@
-let employeeName= 'FirstName, LastName'
-const employeeID= 101
-var isActive= true
+let employeeName= "FirstName, LastName";//String-Name of employee
+const employeeID= 101;//Number-ID assigned to employee
+var isActive= true;//Boolean-True if employee is currently active.
+console.log(employeeName)
+console.log(employeeID)
+console.log(isActive)
 console.log(employeeName,employeeID,isActive)
 console.log(typeof 101)
 
