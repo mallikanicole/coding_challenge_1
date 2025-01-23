4a4656805552d7ce6685444b74583216662459e1
mallikanicole <malmichael23@gmail.com>
2025-01-22 20:24:40 -0500

    Task 1-Employee Info added typeof

--- cc_1.js
+++ cc_1.js
@@ -1,11 +1,13 @@
 let employeeName= "FirstName, LastName";//String-Name of employee
 const employeeID= 101;//Number-ID assigned to employee
 var isActive= true;//Boolean-True if employee is currently active.
-console.log(employeeName)
-console.log(employeeID)
-console.log(isActive)
-console.log(employeeName,employeeID,isActive)
-console.log(typeof 101)
+console.log(employeeName,typeof employeeName)
+console.log(employeeID,typeof employeeID)
+console.log(isActive,typeof isActive)
+console.log(employeeName,employeeID,isActive,)
+
+
+
 
 
 
